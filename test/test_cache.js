8b27c884c5fa93c5ef90e1f03cf6054ef52e5c65
Andy Aylward <aaylward@gmail.com>
2018-05-29 23:22:40 -0400

    tweak tests :potato:

--- test/test_cache.js
+++ test/test_cache.js
@@ -1,37 +1,42 @@
 import test from 'ava';
 import { newLruCache } from '../src/caches.js';
 
-test('you can retrieve items you\'ve stored', t => {
+test('it can retrieve items you\'ve stored', t => {
   const cache = newLruCache(1);
   cache.put("a", 1);
 
   const present = cache.get("a");
   t.true(present.isPresent());
   t.is(present.get(), 1);
+});
 
-  const removedA = cache.remove("a");
-  t.true(removedA);
+test('it can explicitly remove items you\'ve stored', t => {
+  const cache = newLruCache(1);
+  cache.put("a", 1);
+
+  t.true(cache.remove("a"));
   t.false(cache.get("a").isPresent());
+  t.deepEqual(cache.toJsObject(), {}, "empty cache");
   t.is(cache.size(), 0);
 });
 
-test('lru cache evicts oldest item', t => {
+test('it evicts the least recently used oldest key', t => {
   const cache = newLruCache(4);
   cache.put("a", 1);
   cache.put("b", 2);
   cache.put("c", 3);
   cache.put("d", 4);
+  cache.get("a");      // order is now bcda
 
-  t.true(cache.remove("a"));
-
-  cache.put("e", 5);
-  cache.put("f", 6);
+  cache.put("e", 5);   // b should be gone
+  cache.put("f", 6);   // c should be gone
 
   t.false(cache.remove("b"));
+  t.false(cache.remove("c"));
   t.is(cache.size(), 4);
 });
 
-test('lru cache clear empties cache', t => {
+test('it can clear cache', t => {
   const cache = newLruCache(4);
   cache.put("a", 1);
   cache.put("b", 2);
@@ -45,10 +50,20 @@ test('lru cache clear empties cache', t => {
   t.is(cache.size(), 0);
 });
 
-test('lru cache toJsObject works', t => {
+test('it can be converted to a regular JS object', t => {
   const cache = newLruCache(4);
   t.deepEqual(cache.toJsObject(), {}, "empty cache");
 
   cache.put("apples", 42);
+  t.deepEqual(cache.toJsObject(), {"apples": 42}, "cache with stuff");
+});
+
+test('it stores the last value written for a key', t => {
+  const cache = newLruCache(4);
+
+  cache.put("apples", 41);
+  t.is(cache.size(), 1);
+  cache.put("apples", 42);
+  t.is(cache.size(), 1);
   t.deepEqual(cache.toJsObject(), {"apples": 42}, "cache with stuff");
 });
